1166ceb9888c85c860b7aefbc61f95ed8b0e78c4
Lauren Thorud <lthorud12@gmail.com>
2020-05-11 18:43:03 -0500

    first c

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+  stories: ['../src/*.stories.js'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-storysource',
+    '@storybook/addon-backgrounds/register',
+    '@storybook/addon-links/register',
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null
+      }
+    }
+  ],
+};
